76ae07049bdab955d33eb80b8c6ac6c6c605e367
Shaik Jaleel <shaikjaleel270@gmail.com>
2021-02-09 17:20:10 +0530

    test
    Signed-off-by: Shaik Jaleel <shaikjaleel270@gmail.com>

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -3,8 +3,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -113,7 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
